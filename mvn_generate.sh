5d178c09478c44a31ed5cbbde3f1d7f8ea42b2a9
prem <premchandmaddikuntla87@gmail.com>
2024-10-28 18:55:58 +0000

    commit before remove

---
+++ mvn_generate.sh
@@ -0,0 +1,7 @@
+groupId="com.example"
+artifactId="example"
+archetype="maven-archetype-quickstart"
+version="1.4"
+
+mvn archetype:generate -DgroupId=$groupId -DartifactId=$artifactId -DarchetypeArtifactId=$archetype -DarchetypeVersion=1.4 -DinteractiveMode=false
+
